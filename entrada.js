911eabeec46741c02884793f1576bd4e5f70eb8e
Shadow-Slayer <wander.p.c.07@gmail.com>
2018-03-28 20:26:34 -0300

    Add files via upload

---
+++ entrada.js
@@ -0,0 +1,34 @@
+API.on(API.USER_JOIN, user => { 	
+    if (user.id == "4080795")
+    API.sendChat(`/me @${user.username} &lt;3`);
+	
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+	
+    if (user.id == "5039941")
+    API.sendChat(`/me @${user.username} O ceifeiro do ban acaba de adentrar no buteco!`);
+	
+    if (user.id == "631725")
+    API.sendChat(`/me @${user.username} O Pessimista, acabou de chegar!`);
+	
+    if (user.id == "5671563")
+    API.sendChat(`/me @${user.username} :wave: Se abertinha já é abertinha, imagine abertinha??`);
+	
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+	
+    if (user.id == "20472203")
+    API.sendChat(`/me @${user.username} a mais amorzinha da sala chegou! 💋`);
+	
+    if (user.id == "3742812")
+    API.sendChat(`/me @${user.username} Welcome Lord`);
+	
+    if (user.id == "21759495")
+    API.sendChat(`/me @${user.username} The King Of The Kings`);
+	
+    if (user.id == "3180606")
+    API.sendChat(`/me @${user.username} Ranger vermelho chegou, e mofou. Quer dizer... morfou. ;)`);
+	
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+});
